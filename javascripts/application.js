ba0eb6bcc9f0b0a260a616c919955086fbba2c12
Xavier Coulon <xcoulon@redhat.com>
2013-01-10 17:40:52 +0100

    Fixing selected tab on nav bar and removing unused layout

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,28 +2,7 @@
 
   $(function(){
 
-
-    // fix sub nav on scroll
-    var $win = $(window)
-      , $nav = $('.subnav')
-	  , navHeight = $('.navbar').first().height()
-      , navTop = $('.subnav').length && $('.subnav').offset().top - navHeight
-      , isFixed = 0
-
-    processScroll()
-
-    $win.on('scroll', processScroll)
-
-    function processScroll() {
-      var i, scrollTop = $win.scrollTop()
-      if (scrollTop >= navTop && !isFixed) {
-        isFixed = 1
-        $nav.addClass('subnav-fixed')
-      } else if (scrollTop <= navTop && isFixed) {
-        isFixed = 0
-        $nav.removeClass('subnav-fixed')
-      }
-    }
+    $(document.tab).addClass("active");
 
 })
 
