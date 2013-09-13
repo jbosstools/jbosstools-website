3ded82763cfb6515b50afc9354d5e79ed9d1b785
Xavier Coulon <xcoulon@redhat.com>
2013-09-13 10:49:56 +0200

    New downloads layout

--- _ext/downloads.rb
+++ _ext/downloads.rb
@@ -27,11 +27,15 @@ module Awestruct
           eclipse_version = site.eclipse_versions.select{|v| v.id == f.eclipse_requirement}.first
           for stream_type in [:jbds, :jbt_core, :jbt_is]
             stream = f.streams[stream_type]
-            for build_type in [:releases, :development_builds, :nightly_builds]
+            stream_name = site.products[stream_type].name
+            for build_type in [:stable_releases, :development_builds, :nightly_builds]
               downloadable_builds[stream_type] = Hash.new if downloadable_builds[stream_type].nil?
               downloadable_builds[stream_type][build_type] = Array.new if downloadable_builds[stream_type][build_type].nil?
-              downloadable_build = generate_downloadable_build(site, stream, stream[build_type], eclipse_version)
-              downloadable_builds[stream_type][build_type] << downloadable_build unless downloadable_build.nil?
+              downloadable_build = generate_downloadable_build(site, stream_name, stream[build_type], eclipse_version)
+              unless downloadable_build.nil?
+                downloadable_builds[stream_type][build_type] << downloadable_build 
+                puts "Downloadable build: " +  downloadable_build.to_s
+              end
             end
           end
         end
@@ -40,8 +44,8 @@ module Awestruct
         # (each page will provide all downloadable versions at once).
         site.downloads_pages = Hash.new
         for stream_type in [:jbds, :jbt_core, :jbt_is]
-          downloadable_builds[stream_type][:archives] = downloadable_builds[stream_type][:releases].drop(1)
-          downloadable_builds[stream_type][:releases] = Array.[](downloadable_builds[stream_type][:releases].first)
+          downloadable_builds[stream_type][:archives] = downloadable_builds[stream_type][:stable_releases].drop(1)
+          downloadable_builds[stream_type][:stable_releases] = Array.[](downloadable_builds[stream_type][:stable_releases].first)
           download_page = generate_download_page(site, stream_type)
           site.pages << download_page unless download_page.nil?
           site.downloads_pages[stream_type] = download_page unless download_page.nil?
@@ -52,35 +56,24 @@ module Awestruct
       def generate_download_page(site, stream_type)
         product_name = site.products[stream_type].name
         layout_path = @layout_paths[stream_type]
-        puts "Generating download page for " + product_name
         page = find_layout_page(site, layout_path)
+        page.stream_type = stream_type
         page.output_path = File.join( @output_path_prefix + stream_type.to_s + ".html" )
         page.title = product_name
         puts "Generated download page " + page.title + " at " + page.output_path
         return page
       end
       
-      def generate_downloadable_build(site, stream, build, eclipse_version)
+      def generate_downloadable_build(site, stream_name, build, eclipse_version)
         unless build.nil?
           if build.is_a? Array
             build.each do |b|
-              return generate_downloadable_build(site, stream, b, eclipse_version)
+              return generate_downloadable_build(site, stream_name, b, eclipse_version)
             end
           else
-            product = Product.new()
-            product.id = build.id
-            product.name = stream.name
-            product.version = build.version
-            product.release_date = build.release_date
-            product.eclipse_version = eclipse_version
+            product = Product.new(build, stream_name, eclipse_version)
             blog_announcement_page = find_blog_announcement_page(site, build.blog_announcement)
-            unless blog_announcement_page.nil?
-              product.blog_announcement_url = blog_announcement_page.output_path
-            end
-            product.marketplace_install_url = build.marketplace_install_url
-            product.marketplace_url = build.marketplace_url
-            product.update_site_url = build.update_site_url
-            product.zips= build.zips
+            product.blog_announcement_url = blog_announcement_page.output_path unless blog_announcement_page.nil?
             return product
           end
         end 
@@ -116,7 +109,39 @@ module Awestruct
       end
     end
 
-    class Product < Awestruct::AStruct
+    class Product
+      
+      attr_accessor :id
+      attr_accessor :name
+      attr_accessor :version
+      attr_accessor :release_date
+      attr_accessor :eclipse_version
+      attr_accessor :marketplace_install_url
+      attr_accessor :marketplace_url
+      attr_accessor :update_site_url
+      attr_accessor :zips
+      attr_accessor :blog_announcement_url
+      
+      def initialize(build, stream_name, eclipse_version) 
+        @id = build.id
+        @name = stream_name.to_s
+        @version = build.version
+        @release_date = build.release_date.to_s unless build.release_date.nil?
+        @eclipse_version = eclipse_version
+        @marketplace_install_url = build.marketplace_install_url.to_s unless (build.marketplace_install_url.nil? || build.marketplace_install_url.empty?) 
+        @marketplace_url = build.marketplace_url.to_s unless (build.marketplace_url.nil? || build.marketplace_url.empty?)
+        @update_site_url = build.update_site_url.to_s unless (build.update_site_url.nil? || build.update_site_url.empty?)
+        @zips= build.zips unless (build.zips.nil? || build.zips.empty?)
+      end
+      
+      def to_s
+        output = @id + " "
+        output << "(" + @name + ")" unless @name.nil?
+        output << "\n Marketplace: " + @marketplace_url unless @marketplace_url.nil?
+        output << "\n Updatesite: " + @update_site_url unless @update_site_url.nil?
+        output << "\n Zips: " + @zips.to_s unless @zips.nil?
+        return output
+      end 
       
     end
 
--- _ext/downloads_helper.rb
+++ _ext/downloads_helper.rb
@@ -2,21 +2,26 @@ module Awestruct
   module Extensions
     module DownloadsHelper
 
-      def init_download_btn_states()
-        labels = {:releases=>"Lastest Stable Release", :development_builds=>"Development Milestones", :nightly_builds=>"Nightly Builds", :archives=>"Archives"}
-        output = ""
+      def init_download_tab_states(stream_type)
+        labels = {:stable_releases=>"Lastest Stable Release", :development_builds=>"Development Milestones", :nightly_builds=>"Nightly Builds", :archives=>"Archives"}
+        output = "$('#downloadable_versions_tabs a:first').tab('show');\n"
         labels.each_key do |key|
-          unless site.downloadable_builds[:jbt_core][key].empty?
-            site.downloadable_builds[:jbt_core][key].each do |build|
-              if build.marketplace_install_url?
-                output << "$('" + build.id + "_marketplace').addClass('active');\n"
-              else
-                output << "$('" + build.id + "_marketplace').addClass('disabled');\n"
+          unless site.downloadable_builds[stream_type][key].empty?
+            site.downloadable_builds[stream_type][key].each do |build|
+              if build.marketplace_install_url.nil?
+                output << "$('#" + build.id + "_marketplace_tab').addClass('disabled');\n"
+                output << "$('#" + build.id + "_marketplace_tab>a').removeAttr('data-toggle');\n"
               end
-              if build.update_site_url?
+              if build.update_site_url.nil?
+                output << "$('#" + build.id + "_update_site_tab').addClass('disabled');\n"
+                output << "$('#" + build.id + "_update_site_tab>a').removeAttr('data-toggle');\n"
               end
-              if build.zips? && !build.zips.empty?
+              if build.zips.nil?
+                output << "$('#" + build.id + "_zips_tab').addClass('disabled');\n"
+                output << "$('#" + build.id + "_zips_tab>a').removeAttr('data-toggle');\n"
               end
+              output << "$('#" + build.id + "_tabs li:not(.disabled):first>a').tab('show');\n"
+              
             end
           end
         end
