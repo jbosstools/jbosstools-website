0cb3e3caac427aa7dee6f0f06b5bdee426cfb52d
Xavier Coulon <xcoulon@redhat.com>
2013-11-29 19:04:28 +0100

    Work on navbar for downloads + refactoring the yaml model

--- _ext/downloads.rb
+++ _ext/downloads.rb
@@ -4,13 +4,12 @@ module Awestruct
   module Extensions
     class Downloads
 
-
       @@index_path = "/downloads/index.html"
       @@output_path_prefix = "/downloads/"
       @@layout_paths = {:single_version => "download_single_version.html.haml", :all_versions => "download_all_versions.html.haml"}
       @@download_page_metadata = {:latest_release=>{:page_path_fragment=>"latest", :page_title=>"Latest Stable"} ,
           :all_releases=>{:page_path_fragment=>"allversions", :page_title=>"All Versions"}}
-
+      @@build_types = {:stable => [".GA", ".Final"], :development=>[".Alpha", ".Beta", ".CR"], :nightly=>["nightly"]}
 
       def initialize()
       end
@@ -19,88 +18,98 @@ module Awestruct
       def execute(site)
         $LOG.debug "*** Executing downloads extension..." if $LOG.debug?
         @site = site
-        @site.active_download_pages = Hash.new
-        @site.all_download_pages = Hash.new
+        @site.download_pages = Hash.new
+        @site.download_perma_links = Hash.new #permalinks per active eclipse stream.
+        @site.all_versions_download_pages = Hash.new
+        @site.latest_stable_builds_download_pages = Hash.new
         # generate a page for each dev/nightly/stable build per product until a version with a stable build is found 
         # (thus, skipping older product streams),
         # then 1 page for all stable builds (only) per product
-        for product_type in [:devstudio, :devstudio_is, :jbt_core, :jbt_is]
-          @site.active_download_pages[product_type] = Array.new 
-          @site.all_download_pages[product_type] = Array.new
-          if site.products[product_type].nil? then
+        for product in [:devstudio, :devstudio_is, :jbt_core, :jbt_is]
+          @site.download_pages[product] = Hash.new
+          @site.download_perma_links[product] = Hash.new #permalinks per active eclipse stream.
+          if site.products[product].nil? then
             next
           end
-          stable_build_found = false
-          site.products[product_type][:streams].each do |eclipse_stream|
-            eclipse_version = site.products[:eclipse][eclipse_stream[0]]
-            #puts "Eclipse version: " + eclipse_version.to_s
-            # :stable in last position to be sure we grab dev/nightly builds before breaking the loop
-            for build_type in [:development, :nightly,:stable] 
-              if eclipse_stream[1][build_type].nil? || eclipse_stream[1][build_type].first.nil? then
-                next
-              end
-              build_info = eclipse_stream[1][build_type].first
-              #puts " Product build: " + build_info.to_s
-              build_info[1].name = site.products[product_type].name
-              build_info[1].version = build_info[0]
-              #build_info[1].marketplace_url = eclipse_stream[1][:marketplace_url]
-              #build_info[1].download_url = eclipse_stream[1][:download_url]
-              build_info[1].eclipse_version = eclipse_version
-              build_info[1].build_type = build_type
-              
-              unless stable_build_found then
-                page_path_fragment = (build_type == :stable) ? "stable" : build_info[1].version.to_s
-                download_page = generate_single_version_download_page(product_type, eclipse_version, 
-                      page_path_fragment, build_info[1])
-                @site.active_download_pages[product_type] << download_page 
-                @site.pages << download_page 
-                if build_type == :stable then
-                  stable_build_found = true
+          # each product (DevStudio, etc.) is splitted on many Eclipse versions (Luna, etc)
+          site.products[product][:streams].each do |eclipse_stream|
+            eclipse_id = eclipse_stream[0]
+            eclipse_version = site.products[:eclipse][eclipse_id]
+            #permalinks per active eclipse stream.
+            @site.download_perma_links[product][eclipse_id] = Hash.new if eclipse_version.active
+            @site.download_pages[product][eclipse_id] = Array.new
+            #permalinks for "stable.html", "development.html", etc. 
+            puts "Processing " + eclipse_version.full_name + " stream (active: " + eclipse_version.active.to_s + ")..."
+            # for each Eclipse versions can have many product builds, each one with build info
+            eclipse_stream[1].each do |build_version, build_info|
+              build_type = guess_build_type(build_version) 
+              build_info.name = site.products[product].name
+              build_info.version = build_version
+              build_info.eclipse_version = eclipse_version
+              build_info.build_type = build_type
+              if eclipse_version.active && @site.download_perma_links[product][eclipse_id][build_type].nil? then
+                permalink_page = generate_single_version_download_page(product, eclipse_version, 
+                      build_type.to_s, build_info, build_version)
+                @site.download_perma_links[product][eclipse_id][build_type] = permalink_page
+                @site.pages << permalink_page 
+                if @site.latest_stable_builds_download_pages[product].nil? && build_type == :stable then
+                  @site.latest_stable_builds_download_pages[product] = permalink_page
                 end
               end
-              
-              if build_type == :stable then
-                page_path_fragment = build_info[1].version.to_s
-                download_page = generate_single_version_download_page(product_type, eclipse_version, 
-                      page_path_fragment, build_info[1])
-                @site.pages << download_page 
-                @site.all_download_pages[product_type] << download_page 
-              end
+              # build regular download page
+              download_page = generate_single_version_download_page(product, eclipse_version, 
+                    build_version.to_s, build_info, build_version)
+              @site.pages << download_page 
+              @site.download_pages[product][eclipse_id] << download_page 
             end
           end
-          all_versions_page = generate_all_versions_downloads_page(product_type)
+          all_versions_page = generate_all_versions_downloads_page(product)
           @site.pages << all_versions_page
-          @site.active_download_pages[product_type] << all_versions_page
+          @site.all_versions_download_pages[product] = all_versions_page
+          #puts "*** Download permalinks for " + product.to_s + ": " + @site.download_perma_links[product].to_s
         end
         $LOG.debug "*** Done with downloads extension." if $LOG.debug?
       end
 
-      def generate_single_version_download_page(product, eclipse_version, page_path_fragment, build_info)
-        page_subtitle ||= @site.products[product].name + " version " + build_info.version.to_s
-        product_path_fragment = @site.products[product].url_path
-        download_page = generate_download_page(:single_version, product_path_fragment, page_path_fragment)
-        download_page.title = page_subtitle
-        download_page.build_info = build_info
-        download_page.product = product
-        puts " generated download page at '" + download_page.output_path + "' with title '" + download_page.title + "'"
-        download_page
+      def generate_single_version_download_page(product, eclipse_version, page_path_fragment, build_info, build_version)
+        page_title ||= @site.products[product].name + " version " + build_version.to_s
+        product_path_fragment = @site.products[product].url_path_fragment
+        path = @@output_path_prefix + product_path_fragment + "/" + eclipse_version.url_path_fragment + "/" + page_path_fragment + ".html"
+        page = generate_download_page(:single_version, path)
+        page.title = page_title
+        page.build_info = build_info
+        page.product = product
+        page.eclipse_version = eclipse_version
+        puts "  generated download page at '" + page.output_path + "' with title '" + page.title + "'"
+        page
       end
 
       def generate_all_versions_downloads_page(product)
-        product_path_fragment = @site.products[product].url_path
-        download_page = generate_download_page(:all_versions, product_path_fragment, @@download_page_metadata[:all_releases][:page_path_fragment])
-        download_page.title = @@download_page_metadata[:all_releases][:page_title]
+        product_path_fragment = @site.products[product].url_path_fragment
+        path = @@output_path_prefix + product_path_fragment + "/" + @@download_page_metadata[:all_releases][:page_path_fragment] + ".html"
+        download_page = generate_download_page(:all_versions, path)
+        download_page.title = @site.products[product].name + " - " + @@download_page_metadata[:all_releases][:page_title]
         download_page.product = product
         puts " generated download page at '" + download_page.output_path + "' with title '" + download_page.title + "'"
         download_page
       end
     
-      def generate_download_page(layout_type, product_path_fragment, page_path_fragment)
+      def generate_download_page(layout_type, path)
         layout_path = @@layout_paths[layout_type]
         page = find_layout_page(layout_path)
-        page.output_path = File.join( @@output_path_prefix + product_path_fragment + "/" + page_path_fragment + ".html" )
+        page.output_path = File.join(path)
         @site.pages << page
         return page
+      end
+      
+      def guess_build_type(build_version)
+        @@build_types.each do |type, suffixes| 
+          unless suffixes.select{|suffix| build_version.include? suffix}.first.nil?
+            return type
+          end
+        end
+        puts "Unable to determine build type for " + build_version + ", assuming :development, then.."
+        return :development
       end
       
       def find_blog_announcement_page(blog_announcement_page_name)
