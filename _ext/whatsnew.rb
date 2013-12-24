787028b9ebd0c5f43a2815dfe891266ed39c2c3e
Xavier Coulon <xcoulon@redhat.com>
2013-12-24 15:41:41 +0100

    Fixing LiveReload N&N, styling N&N page

--- _ext/whatsnew.rb
+++ _ext/whatsnew.rb
@@ -20,42 +20,40 @@ module Awestruct
           site.product_whatsnews = product_whatsnews
           site.pages.each do |page|
             if page.relative_source_path =~ /^#{@path_prefix}\/.*\.adoc/ then
-              unless site.features[page.feature_id].nil?
-                $LOG.debug " Processing N&N " + page.feature_id.to_s +  " " + page.feature_version.to_s if $LOG.debug?
-                puts " Processing N&N for " + page.feature_id.to_s + " " + page.feature_version
-                whatsnew = OpenStruct.new
-                site.engine.set_urls([page])
-                whatsnew.url = URIHelper.concat(site.base_url, page.url)
-                whatsnew.feature_id = page.feature_id
-                whatsnew.product_version = page.jbt_core_version
-                whatsnew.feature_name = page.feature_name
-                whatsnew.feature_version = page.feature_version
-                whatsnew.output_path = page.output_path
-                whatsnew.content = page.content
-                if module_whatsnews[whatsnew.feature_id].nil? then 
-                  module_whatsnews[whatsnew.feature_id] = Hash.new
-                end
-                if product_whatsnews[whatsnew.product_version].nil? then
-                  product_whatsnews[whatsnew.product_version] = Array.new
-                end
-                product_whatsnews[whatsnew.product_version] << whatsnew
-                
-                main_version = get_main_version(whatsnew.feature_version)
-                if module_whatsnews[whatsnew.feature_id][main_version].nil? then
-                  #puts " Adding " + page.feature_id + " version " + main_version
-                  whatsnew_page = create_page(page.feature_id, main_version + ".html")
-                  whatsnew_page.feature_version = main_version
-                  whatsnew_page.feature_id = page.feature_id
-                  whatsnew_page.title = site.features[whatsnew_page.feature_id].name + " " + main_version
-                  module_whatsnews[whatsnew.feature_id][main_version] = whatsnew_page
-                  # TODO: how to append page.content into whatsnew_page.content at the right location ?
-                  #generate front-matter + content in raw_content ?
-                  #puts page.content
-                  module_whatsnews[whatsnew.feature_id][main_version].items = Array.new
-                end
-                #puts " Page content: #{page.content[0..10]}..."
-                module_whatsnews[whatsnew.feature_id][main_version].items << page.content
+              $LOG.debug " Processing N&N " + page.feature_id.to_s +  " " + page.feature_version.to_s if $LOG.debug?
+              puts " Processing N&N for " + page.feature_id.to_s + " " + page.feature_version
+              whatsnew = OpenStruct.new
+              site.engine.set_urls([page])
+              whatsnew.url = URIHelper.concat(site.base_url, page.url)
+              whatsnew.feature_id = page.feature_id
+              whatsnew.product_version = page.jbt_core_version
+              whatsnew.feature_name = page.feature_name
+              whatsnew.feature_version = page.feature_version
+              whatsnew.output_path = page.output_path
+              whatsnew.content = page.content
+              if module_whatsnews[whatsnew.feature_id].nil? then 
+                module_whatsnews[whatsnew.feature_id] = Hash.new
               end
+              if product_whatsnews[whatsnew.product_version].nil? then
+                product_whatsnews[whatsnew.product_version] = Array.new
+              end
+              product_whatsnews[whatsnew.product_version] << whatsnew
+              
+              main_version = get_main_version(whatsnew.feature_version)
+              if module_whatsnews[whatsnew.feature_id][main_version].nil? then
+                #puts " Adding " + page.feature_id + " version " + main_version
+                whatsnew_page = create_page(page.feature_id, main_version + ".html")
+                whatsnew_page.feature_version = main_version
+                whatsnew_page.feature_id = page.feature_id
+                whatsnew_page.title = site.modules[whatsnew_page.feature_id].name + " " + main_version
+                module_whatsnews[whatsnew.feature_id][main_version] = whatsnew_page
+                # TODO: how to append page.content into whatsnew_page.content at the right location ?
+                #generate front-matter + content in raw_content ?
+                #puts page.content
+                module_whatsnews[whatsnew.feature_id][main_version].items = Array.new
+              end
+              #puts " Page content: #{page.content[0..10]}..."
+              module_whatsnews[whatsnew.feature_id][main_version].items << page.content
             end
             # sort the hashes by their key in reversed order (more recent versions come first)
             module_whatsnews.each do |feature_id, module_whatsnew|
