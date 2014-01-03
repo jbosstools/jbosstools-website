d265c31cc61881880ec327c60f544be6e442b2a7
Xavier Coulon <xcoulon@redhat.com>
2014-01-03 11:36:02 +0100

    Fixing broken links on feature->other feature links in sidebar

--- _ext/myposts.rb
+++ _ext/myposts.rb
@@ -23,13 +23,13 @@ module Awestruct
         site.posts.each do |post|
           #todo why is this not just the pageurl so relative urls just works ?
           #post.imagesdir = URIHelper.concat(site.base_url, @images_dir)
-          puts "post " + post.to_s
+          #puts "post " + post.to_s
           #post.url = URIHelper.concat(site.base_url, post.output_path)
           #puts post.title + ": " + post.output_path.to_s + " -> " + post.url
           if ( post.relative_source_path =~ /^#{@path_prefix}\/([^.]+)\..*$/ ) then
             basename=$1
             post.output_path="#{@path_prefix}/#{basename}.html"
-            puts "post -> " + post.output_path.to_s
+            #puts "post -> " + post.output_path.to_s
           end
         end
         $LOG.debug "*** Done executing posts extension." if $LOG.debug?
