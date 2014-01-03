762b523742a6007222ef25d2311e6628e7abb358
Xavier Coulon <xcoulon@redhat.com>
2014-01-03 11:36:02 +0100

    Fixing broken links on feature->other feature links in sidebar

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -44,7 +44,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::DataDir.new
   extension Awestruct::Extensions::DataDir.new('/whatsnew') # no '/', "What's New" pages will be under other categories. 
   extension Awestruct::Extensions::DataDir.new('/features')
-  extension Awestruct::Extensions::MyPosts.new('/blog', :posts, nil, nil, :imagesdir => '/blog/images')
+  extension Awestruct::Extensions::MyPosts.new('/blog', :posts)
   extension Awestruct::Extensions::MyPaginator.new(:posts, '/blog/index', :per_page => 2 )
   extension Awestruct::Extensions::MyTagger.new( :posts, '/blog/index', '/blog/tags', :per_page=>10,
    :sanitize=>true )
