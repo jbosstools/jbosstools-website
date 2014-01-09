2c10d5c74b3c125e4acc3718d88e0edc89dfb8fe
Xavier Coulon <xcoulon@redhat.com>
2014-01-09 16:45:28 +0100

    Fixing missing update site URLs on download pages (giving bad links)

--- _ext/downloads.rb
+++ _ext/downloads.rb
@@ -38,7 +38,7 @@ module Awestruct
             @site.download_perma_links[product][eclipse_id] = Hash.new if eclipse_version.active
             @site.download_pages[product][eclipse_id] = Array.new
             #permalinks for "stable.html", "development.html", etc. 
-            #puts "Processing " + eclipse_version.full_name + " stream (active: " + eclipse_version.active.to_s + ")..."
+            puts "Processing " + eclipse_version.full_name + " stream (active: " + eclipse_version.active.to_s + ")..."
             # for each Eclipse versions can have many product builds, each one with build info
             eclipse_stream.each do |build_version, build_info|
               build_type = guess_build_type(build_version) 
@@ -47,6 +47,8 @@ module Awestruct
               build_info.eclipse_version = eclipse_version
               build_info.build_type = build_type
               build_info.blog_announcement_url = (defined? build_version.blog_announcement) ? find_blog_announcement_path(build_version.blog_announcement) : nil
+              build_info.update_site_url = (defined? build_version.update_site_url) ? update_site_url : nil
+              puts "  update site:" + build_info.update_site_url.to_s
               if eclipse_version.active && @site.download_perma_links[product][eclipse_id][build_type].nil? then
                 permalink_page = generate_single_version_download_page(product, eclipse_version, 
                       build_type.to_s, build_info, build_version)
