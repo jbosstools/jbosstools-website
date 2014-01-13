e7bc503bc093136a9c5fb61524e9c7b6da155c14
Xavier Coulon <xcoulon@redhat.com>
2014-01-13 21:31:42 +0100

    Work on download requirements and more update site zips

--- _ext/downloads.rb
+++ _ext/downloads.rb
@@ -45,11 +45,13 @@ module Awestruct
               build_info.name = site.products[product].name
               build_info.version = build_version
               build_info.eclipse_version = eclipse_version
+              build_info.requirements = (defined? site.products[eclipse_version].requirements) ? site.products[eclipse_version].requirements : nil
+              puts "  Requirements: " + build_info.requirements.to_s
               build_info.build_type = build_type
               build_info.blog_announcement_url = (defined? build_version.blog_announcement) ? find_blog_announcement_path(build_version.blog_announcement) : nil
               build_info.update_site_url = (defined? build_version.update_site_url) ? build_version.update_site_url : nil
               build_info.marketplace_install_url = (defined? build_version.marketplace_install_url) ? build_version.marketplace_install_url : nil
-              puts "  update site:" + build_info.update_site_url.to_s + " marketplace_install_url=" + build_info.marketplace_install_url.to_s
+              puts "  Update Site=" + build_info.update_site_url.to_s + " / Marketplace=" + build_info.marketplace_install_url.to_s
               if eclipse_version.active && @site.download_perma_links[product][eclipse_id][build_type].nil? then
                 permalink_page = generate_single_version_download_page(product, eclipse_version, 
                       build_type.to_s, build_info, build_version)
