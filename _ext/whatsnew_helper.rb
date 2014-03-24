5a5d28fa2bd109d40cac1702ccf13bf2541e4838
Xavier Coulon <xcoulon@redhat.com>
2014-03-24 16:51:34 +0100

    JBIDE-16843 - Restore /documentation/whatsnew (drop the '_') and get individual pages generated
    Fixed missing pages on sidenavbar
    Reduced number of versions labelled with "Stable" / "Development" / "Nightly", shared code
    between "Downloads" and "Whatsnew" parts.
    Added multiple versions on /documentation/whatsnew
    Moved remaining article from /documentation/_whatsnew to /documentation/whatsnew

--- _ext/whatsnew_helper.rb
+++ _ext/whatsnew_helper.rb
@@ -1,19 +1,18 @@
+require 'products_helper'
+
 module Awestruct
   module Extensions
     module WhatsnewHelper
       
-      def get_main_version(version)
-        version.split(".")[0..2].join('.')
+      # returns the list of pages that will appear in the sidenavbar
+      def get_active_whatsnew_pages(site, product_id, product_version) 
+        site.whatsnew_pages[product_id].values.select{|p| p.product_active == true && (is_final_version(p.product_version) || !exists_final_version_whatsnew_page(site, p.product_id, p.product_version))}
       end
-      module_function :get_main_version
       
       # Returns the list of pages whose version match the given version (minus the qualifier) 
       def get_related_whatsnew_pages(site, product_id, product_version)
         main_version = get_main_version(product_version)
-        puts "Looking for whatsnew pages related with #{product_id}.#{product_version}..."
-        result = site.whatsnew_pages[product_id].select{|version, p| (version.start_with? main_version) && (version != product_version)}
-        puts " found #{result.length} pages"
-        result
+        site.whatsnew_pages[product_id].select{|version, p| (version.start_with? main_version) && (version != product_version)}
       end
       
       def get_final_version_whatsnew_page(site, product_id, product_version)
@@ -22,28 +21,23 @@ module Awestruct
         site.whatsnew_pages[product_id].values.select{|p| p.product_version == final_version}.first
       end
 
-      def exists_final_version(site, product_id, product_version)
+      def get_current_version_whatsnew_page(site, product_id, product_version)
+        site.whatsnew_pages[product_id].values.select{|p| p.product_version == product_version}.first
+      end
+      
+      def get_aggregate_whatsnew_page(site, product_id, product_version)
+        if exists_final_version_whatsnew_page(site, product_id, product_version)
+          get_final_version_whatsnew_page(site, product_id, product_version)
+        else
+          aggregated_whatsnew_page = get_current_version_whatsnew_page(site, product_id, product_version)
+        end
+      end
+      
+      def exists_final_version_whatsnew_page(site, product_id, product_version)
         #puts " Checking if final version for #{product_id}.#{product_version} exists: #{!(get_final_version_whatsnew_page(site, product_id, product_version).nil?)}"
         return !(get_final_version_whatsnew_page(site, product_id, product_version).nil?)
       end
       
-      def get_final_version(site, product_id, version) 
-        main_version = get_main_version(version)
-        final_version = main_version << ".Final"
-        site.products[product_id].streams.each do |stream_id, product_versions|
-          if product_versions.include? final_version
-            return final_version
-          end
-        end
-        #puts "   Final version for #{product_id} (#{version}): #{final_version} does not exist yet."
-        nil
-      end
-      module_function :get_final_version
-      
-      def is_final_version(product_version)
-        product_version.end_with? ".Final"  
-      end
-      module_function :is_final_version
     end
   end
 end
