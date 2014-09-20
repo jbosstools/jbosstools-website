8e3b64ec0426e49e4339b0451a41625975455251
Max Rydahl Andersen <manderse@redhat.com>
2014-09-20 12:12:19 +0200

    Revert "JBIDE-18390 - Updating with latest changes in bootstrap community theme"
    This reverts commit 164e42348c7cc66470155dffb3afad4f178f27ca.

--- _ext/file_merger.rb
+++ _ext/file_merger.rb
@@ -24,7 +24,7 @@
 #        - /javascripts/prettify.js
 #        - /javascripts/bootstrap-transition.js
 #        - /javascripts/bootstrap-alert.js
-#
+#        
 ##
 
 module Awestruct
@@ -59,6 +59,7 @@ module Awestruct
         # Create a temporary file with the merged content.
         tmpOutputPath = File.join( "./_tmp/" , File.basename(outputPath))
         tmpOutputFile = File.new(tmpOutputPath,"w")
+        puts "Merging files into " + tmpOutputPath.to_s
         tmpOutputFile.write(output)
         tmpOutputFile.close
 
