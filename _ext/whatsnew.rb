043521635a046befb25a4eae91138d0f1b32ddef
Xavier Coulon <xcoulon@redhat.com>
2015-10-05 10:38:37 +0200

    Fixed missing Docker tools in 4.3.0.Final N&N

--- _ext/whatsnew.rb
+++ _ext/whatsnew.rb
@@ -42,13 +42,13 @@ module Awestruct
             end
             whatsnew_page = get_whatsnew_page(site, component_page.product_id, component_page.product_version)
             add_component_page(whatsnew_page, component_page)
-            
+
             # now, deal with *.Final* versions if they exist in site.products
             # if the .Final.adoc page exists and contains a "include-previous" header set to false, then no aggregation is
             # performed (see https://issues.jboss.org/browse/JBIDE-20851)
-            unless Products_Helper.is_stable_version(component_page.component_version)
+            unless Products_Helper.is_stable_version(component_page.product_version)
               product_stable_version = Products_Helper.get_stable_version(site, component_page.product_id, component_page.product_version)
-              puts "    Stable version: #{product_stable_version}"
+              #puts "    Stable version: #{product_stable_version}"
 
               if product_stable_version.nil?
                 puts "    Skipping aggregation of #{component_page.product_id}.#{component_page.product_version} page into stable version since it does not exist yet"
@@ -61,6 +61,8 @@ module Awestruct
                   add_component_page(whatsnew_final_page, component_page)
                 elsif !stable_component_page.nil?
                   puts "    Skipping aggregation because 'include-previous' page attribute was set to 'false'"
+                else
+                  puts "    Skipping aggregation no 'stable' N&N page was found"
                 end
               end
             end
@@ -105,7 +107,7 @@ module Awestruct
       def get_whatsnew_page(site, product_id, product_version)
         site.whatsnew_pages[product_id] = Hash.new if site.whatsnew_pages[product_id].nil?
         if site.whatsnew_pages[product_id][product_version].nil? then
-          puts "Building N&N page for #{product_id} #{product_version}"
+          #puts "Building N&N page for #{product_id} #{product_version}"
           product_url_path_fragment = site.products[product_id].url_path_fragment
           page = create_page(site, @@whatsnew_layout_path, @target_path_prefix, product_url_path_fragment, product_version)
           page.build_info = Products_Helper.get_product_info(site, product_id, product_version)
